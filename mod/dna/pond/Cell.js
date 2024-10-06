f12f676ed4634e4ff966d1d9508a035242ba1de7
Anatolii Yakushko <anatolii.yakushko@megafair.io>
2024-10-06 14:22:13 +0300

    [+] added random cell images

--- mod/dna/pond/Cell.js
+++ mod/dna/pond/Cell.js
@@ -1,14 +1,47 @@
 class Cell {
 
     constructor(st) {
+        let cellTypes = {
+            'froggy': {
+                img: res.cell.froggy,
+                w: 512,
+                h: 276,
+                centers: [
+                    {
+                        x: 50,
+                        y: 50,
+                        r: 25
+                    }
+                ]
+            },
+            'jelly': {
+                img: res.cell.jelly,
+                w: 512,
+                h: 412,
+                centers: [
+                    {
+                        x: 50,
+                        y: 50,
+                        r: 25
+                    }
+                ]
+            }
+        }
+        
         extend(this, {
             x: 0,
             y: 0,
             r: 20,
-
+            a: 0,
+            da: (Math.random() * 0.4) * math.rnds(),
             dx: 10,
             dy: 10,
+            descriptor: math.rnde(cellTypes)
         }, st)
+        this.aspectRate = this.descriptor.w / this.descriptor.h
+        this.w = this.r * 2
+        this.h = this.w / this.aspectRate
+        console.log("descriptor", this.descriptor);
     }
 
     init() {
@@ -19,6 +52,7 @@ class Cell {
     }
 
     evo(dt) {
+        this.a += this.da * dt
         const R = this.r
         this.x += this.dx * dt
         this.y += this.dy * dt
@@ -28,9 +62,16 @@ class Cell {
         else if (this.y > ctx.height-R && this.dy > 0) this.dy *= -1
     }
 
+
+
     draw() {
+        save();
+        translate(this.x, this.y);
+        rotate(this.a);
+        image(this.descriptor.img, - this.w / 2, - this.h / 2, this.w, this.h);
         lineWidth(2)
         stroke('#40A0CE')
-        circle(this.x, this.y, this.r)
+        circle(0, 0, this.r)
+        restore();
     }
 }
---
+++ mod/lab/screen/title/cellTitle.js
@@ -0,0 +1,3 @@
+function draw(){
+    image(res.cell.froggy, rx(0.5) - 50, ry(0.7) - 50, 100, 100)
+}
