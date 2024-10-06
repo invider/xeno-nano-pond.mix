b0d753ed99276ca91761fea75228c332d56b5fba
Igor Khotin <khotin@gmx.com>
2024-10-06 21:57:44 +0300

    [*] object picking on mouse click

--- mod/trap/mouseDown.js
+++ mod/trap/mouseDown.js
@@ -1,15 +1,23 @@
 function mouseDown(e) {
     if (env.screen === 'pond') {
-        log(`look at port::${e.pageX}:${e.pageY}`)
+        const sx = e.pageX,
+              sy = e.pageY
+        log(`look at port::${sx}:${sy}`)
         lib.sfx('pick')
         lab.pond.setViewTarget(e.pageX, e.pageY)
 
-        const lx = lab.pond.lx(e.pageX),
-              ly = lab.pond.ly(e.pageY)
-        const smellDir = lab.pond.smellMap.getSmellDir(lab.pond.smellMap.foodMap, lx, ly)
+        const gx = lab.pond.lx(sx),
+              gy = lab.pond.ly(sy)
+        const smellDir = lab.pond.smellMap.getSmellDir(lab.pond.smellMap.foodMap, gx, gy)
         if (smellDir) {
             log(`smell dir: [${smellDir.dx}:${smellDir.dy}]`)
         }
+
+        const picked = []
+        const node = lab.pond.pick(sx, sy, picked)
+        picked.forEach(node => {
+            console.dir(node)
+        })
     }
 }
 
