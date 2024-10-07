ce5d1a62ede377b7f9a9b555cff24a0ebef13236
Anatolii Yakushko <anatolii.yakushko@megafair.io>
2024-10-07 03:20:45 +0300

    [=] small refactor

--- mod/dna/pond/Cell.js
+++ mod/dna/pond/Cell.js
@@ -1,85 +1,5 @@
-const cellTypes = {
-    'froggy': {
-        img: res.cell.froggy,
-        lifespan: 10,
-        baseHp: 200,
-        team: 1,
-        cellType: 'froggy',
-        centers: [
-            {
-                x: 0,
-                y: 0,
-                r: 16
-            }
-        ]
-    },
-    'jelly': {
-        img: res.cell.jelly,
-        lifespan: 40,
-        team: 2,
-        baseHp: 60,
-        cellType: 'jelly',
-        centers: [
-            {
-                x: 0,
-                y: 0,
-                r: 16
-            }
-        ]
-    },
-    'orange': {
-        img: res.cell.orangy,
-        lifespan: 20,
-        baseHp: 80,
-        cellType: 'orange',
-        team: 3,
-        centers: [
-            {
-                x: 0,
-                y: 0,
-                r: 16
-            }
-        ]
-    },
-    'swampy': {
-        img: res.cell.swampy,
-        lifespan: 15,
-        baseHp: 150,
-        cellType: 'swampy',
-        team: 4,
-        centers: [
-            {
-                x: 0,
-                y: 0,
-                r: 16
-            }
-        ]
-    },
-    'brownie': {
-        img: res.cell.brownie,
-        lifespan: 30,
-        baseHp: 90,
-        team: 5,
-        cellType: 'brownie',
-        centers: [
-            {
-                x: 0,
-                y: 0,
-                r: 16
-            }
-        ]
-    }
-}
-function getMaxHp() {
-    let maxHp = 0;
-    for (let k in cellTypes) {
-        maxHp = Math.max(cellTypes[k].baseHp, maxHp);
-    }
-    return maxHp
-}
-const maxHp = getMaxHp();
-
 let _id = 0
+
 class Cell {
 
     constructor(st) {
@@ -101,7 +21,7 @@ class Cell {
             dx: 10,
             dy: 10,
             damage: 30,
-            descriptor: math.rnde(cellTypes)
+            descriptor: math.rnde(dna.pond.cellConfig.cellTypes)
         }, st)
         this.team = this.descriptor.team
         this.name = 'cell' + (++_id) + 't' + this.team
@@ -280,7 +200,7 @@ class Cell {
             // hp bar
             lineWidth(2)
             stroke('#ff0000')
-            var lifespanWidth = this.w * this.hp / maxHp
+            var lifespanWidth = this.w * this.hp / dna.pond.cellConfig.maxHp
             line(- halfW, - halfH, - halfW + lifespanWidth, - halfH)
 
             // hp bar
